738e69c5f5846ea093fa88c77d580259a0ba18c2
R Herick Fauzi Komara Kusumah <r.herick.fkk@gmail.com>
2026-03-27 09:01:08 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>R Herick Fauzi Komara Kusumah</h1>
+    <h2>Seorang pria dengan mimpi yang besar</h2>
+</body>
+</html>
